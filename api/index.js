5dfd036538f4d88f303efc511fa6ba588206ba41
isen-sama <arnakorn567@gmail.com>
2025-01-22 04:11:14 +0700

    first commit

--- api/index.js
+++ api/index.js
@@ -4,10 +4,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
 
 // Firebase Realtime Database URL
 const databaseURL = process.env.DATABASE_URL;
+
+// Google Cloud Service Account Credentials
+const serviceAccount = {
+  type: process.env.GC_TYPE,
+  project_id: process.env.GC_PROJECT_ID,
+  private_key_id: process.env.GC_PRIVATE_KEY_ID,
+  private_key: process.env.GC_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GC_CLIENT_EMAIL,
+  client_id: process.env.GC_CLIENT_ID,
+  auth_uri: process.env.GC_AUTH_URI,
+  token_uri: process.env.GC_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GC_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GC_CLIENT_X509_CERT_URL
+};
 
 // LINE API Config
 const LINE_CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
